625f23ba5385cb63735206549f5dbbd5da7e6745
Brian Diesel <bri@ndiesel.net>
2025-01-21 17:59:10 -0500

    Initial Commit

---
+++ mapkit-bucket-list/mapkit_bucket_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapkit_bucket_listApp.swift
+//  mapkit-bucket-list
+//
+//  Created by Brian Diesel on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct mapkit_bucket_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
