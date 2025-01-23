52cfa3a7cc410f2c67aed07399b23c482d2c47f1
Brian Diesel <bri@ndiesel.net>
2025-01-23 16:20:00 -0500

    Add sample data to location struct

--- mapkit-bucket-list/Location.swift
+++ mapkit-bucket-list/Location.swift
@@ -21,4 +21,8 @@ struct Location: Codable, Equatable, Identifiable {
     static func ==(lhs: Location, rhs: Location) -> Bool {
         lhs.id == rhs.id
     }
+    
+    #if DEBUG
+        static let example = Location(id: UUID(), name: "The Old Belfry", description: "On the early morning of April 19, 1775, the Belfry rang its bell, summoning the local militia to assemble on the Common to be prepared for the arrival of British soldiers.", latitude: 42.44822, longitude: 71.23085)
+    #endif
 }
