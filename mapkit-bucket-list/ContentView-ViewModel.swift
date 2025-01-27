5e07c1e20177f42c03ec11888f3350cf54dd7b35
Brian Diesel <bri@ndiesel.net>
2025-01-27 11:18:31 -0500

    Add biometrics to unlock the device

--- mapkit-bucket-list/ContentView-ViewModel.swift
+++ mapkit-bucket-list/ContentView-ViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import LocalAuthentication
 import MapKit
 
 extension ContentView {
@@ -13,6 +14,7 @@ extension ContentView {
     class ViewModel {
         private(set) var locations: [Location]
         var selectedPlace: Location?
+        var isUnlocked = false
 
         let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
         
@@ -46,5 +48,26 @@ extension ContentView {
                 print("Unable to save data.")
             }
         }
+
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places."
+
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // no biometrics
+            }
+        }
+        
     }
 }
--- mapkit-bucket-list/ContentView.swift
+++ mapkit-bucket-list/ContentView.swift
@@ -17,33 +17,41 @@ struct ContentView: View {
             span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         )
     )
-    
+   
     var body: some View {
-        MapReader { proxy in
-            Map(initialPosition: startPosition) {
-                ForEach(viewModel.locations) { location in
-                    Annotation(location.name, coordinate: location.coordinate) {
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundStyle(.red)
-                            .frame(width: 44, height: 44)
-                            .background(.white)
-                            .clipShape(.circle)
-                            .simultaneousGesture(LongPressGesture(minimumDuration: 0.5).onEnded { _ in
-                                viewModel.selectedPlace = location }
-                            )
+        if viewModel.isUnlocked {
+            MapReader { proxy in
+                Map(initialPosition: startPosition) {
+                    ForEach(viewModel.locations) { location in
+                        Annotation(location.name, coordinate: location.coordinate) {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(.circle)
+                                .simultaneousGesture(LongPressGesture(minimumDuration: 0.5).onEnded { _ in
+                                    viewModel.selectedPlace = location }
+                                )
+                        }
+                    }
+                }.onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addLocation(at: coordinate)
                     }
                 }
-            }.onTapGesture { position in
-                if let coordinate = proxy.convert(position, from: .local) {
-                    viewModel.addLocation(at: coordinate)
+                .sheet(item: $viewModel.selectedPlace) { place in
+                    EditMarkerView(location: place) {
+                        viewModel.update(location: $0)
+                    }
                 }
             }
-            .sheet(item: $viewModel.selectedPlace) { place in
-                EditMarkerView(location: place) {
-                    viewModel.update(location: $0)
-                }
-            }
+        } else {
+            Button("Unlock Places", action: viewModel.authenticate)
+                .padding()
+                .background(.blue)
+                .foregroundStyle(.white)
+                .clipShape(.capsule)
         }
     }
 }
